94847831265c7d25d2e1e5425a11aad1f2ef1762
yirp <yrphaoma@163.com>
2014-05-12 17:59:13 +0800

    add  two jars :'mina-transport-apr-2.0.7.jar', 'tomcat-apr-5.5.23.jar', used to apr example

---
+++ zemo/src/main/java/com/eric/mina/test/server/apr/MinaTimeServer.java
@@ -0,0 +1,37 @@
+package com.eric.mina.test.server.apr;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.apr.AprSocketAcceptor;
+
+public class MinaTimeServer {
+	private static final int PORT = 9123;
+
+	public static void main(String[] args) throws IOException {
+		// 创建服务器监听
+		IoAcceptor acceptor = new AprSocketAcceptor();
+		// 增加日志过滤器
+		acceptor.getFilterChain().addLast("logger", new LoggingFilter());
+		// 增加编码过滤器
+		acceptor.getFilterChain().addLast(
+				"codec",
+				new ProtocolCodecFilter(new TextLineCodecFactory(Charset
+						.forName("UTF-8"))));
+
+		// 指定业务逻辑处理器
+		acceptor.setHandler(new TimeServerHandler());
+		// 设置buffer的长度
+		acceptor.getSessionConfig().setReadBufferSize(2048);
+		// 设置连接超时时间
+		acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
+		// 绑定端口
+		acceptor.bind(new InetSocketAddress(PORT));
+	}
+}
